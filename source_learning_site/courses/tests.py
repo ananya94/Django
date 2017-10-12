b92066f05cb470e8c378227b5cf66ae44be84add
ananya94 <ananya.2394@gmail.com>
2017-10-12 11:47:01 -0500

    testing the models

--- source_learning_site/courses/tests.py
+++ source_learning_site/courses/tests.py
@@ -1,3 +1,36 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.utils import timezone
+from .models import Course,Step
 
 # Create your tests here.
+
+class CourseModelTest(TestCase):
+    def test_course_creation(self):
+        course = Course.objects.create(
+            title = "Python Regex",
+            description = "Learn to write regular expressions in Python"
+        )
+        now = timezone.now()
+        self.assertLessEqual(course.created_at, now)
+
+class CourseViewsTests(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+        title = "Python testing",
+        description = "learn how to test in python"
+        )
+        self.course2 = Course.objects.create(
+        title= "New Course",
+        description = "A new course"
+        )
+        self.step = Step.objects.create(
+            title="Intro to doctest",
+            description = "learn to write tests in docstrings",
+            course=self.course
+        )
+    def test_course_list_view(self):
+        resp=self.client.get(reverse('courses:list'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(self.course,resp.context['courses'])
+        self.assertIn(self.course2,resp.context['courses'])
